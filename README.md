203e5cbd231996d925e3f271ce3d5ec82a9ef4cc
amanda-w <amandanwong@gmail.com>
2022-08-05 21:17:27 -0700

    completed game

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Project-1
+# SPACE SNAKE
+
+SEIRFX 621 - PROJECT 1
+
+### INSTRUCTIONS
+This is the classic game of Snake, but Space Edition! Use the arrow keys - up, down, left, and right to move the snake to collect as many stars as possible! But be careful...don't run into any walls or crash into yourself or it will be game over!
+#
+
+### WIRE FRAME
+I started with a wire frame to get an idea of how I wanted my game layout to look.
+
+![Wire Frame](/img/Project%201%20Wire%20Frame.png)
+#
+
+### FEATURES TO ADD
+I would like to add a restart button that would reset the game after losing and a high score field that would save the highest score a user achieved.
+#
+
+### INSTALLATION INSTRUCTIONS
+1. `Fork` and `Clone` this repository
+2. Open `index.html` to the browser
+#
--- main.js
+++ main.js
@@ -1,38 +1,126 @@
 let canvas = document.getElementById('game');
 let ctx = canvas.getContext('2d');
 
-let speed = 5;
+let speed = 10;
 
 let blockCount = 20;
 let blockSize = canvas.width / blockCount;
 let headX = 10;
 let headY = 10;
+const snakeBody = [];
+let tail = 2;
 
 let xSpeed = 0;
 let ySpeed = 0;
 
+// food
+let foodX = 3;
+let foodY = 3;
+
 let score = document.getElementById('score');
+
 let highScore = document.getElementById('highscore');
+
+// Entitites
+class Crawler {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+        this.render = function() {
+            context.fillstyle = this.color;
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+}
 
 // Game Process
 function drawGame() {
     // console.log('draw game');
-    clearScreen();
     changePosition();
+    let crashWall = gameOver();
+    if (crashWall) {
+        return;
+    }
+    clearScreen();
+    collision();
+    getFood();
     drawSnake();
     setTimeout(drawGame, 1000 / speed);
 }
 
-drawGame();
+
+
+function gameOver() {
+    let gameEnds = false;
+
+    if (ySpeed === 0 && xSpeed === 0) {
+        return false;
+    }
+
+    if (headX < 0) {
+        gameEnds = true;
+    } else if (headX === blockCount) {
+        gameEnds = true;
+    } else if (headY < 0) {
+        gameEnds = true;
+    } else if (headY === blockCount) {
+        gameEnds = true;
+    }
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        let body = snakeBody[i];
+        if (body.x === headX && body.y === headY) {
+            gameEnds = true;
+            break;
+        }
+    }
+
+
+    if (gameEnds) {
+        ctx.fillStyle = 'white';
+        ctx.font = '50px Copperplate';
+        ctx.fillText('You Lose!', 100, 200);
+    }
+
+    return gameEnds;
+}
 
 function clearScreen() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+function collision() {
+    if (foodX === headX && foodY === headY) {
+    foodX = Math.floor(Math.random() * blockCount);
+    foodY = Math.floor(Math.random() * blockCount);
+    tail++;
+    let newScore = Number(score.textContent) + 1;
+    score.textContent = newScore;
+    }
+}
+
+function getFood() {
+    ctx.fillStyle = 'navajowhite';
+    ctx.fillRect(foodX * blockCount, foodY * blockCount, blockSize, blockSize);
+}
+
 function drawSnake() {
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(headX * blockCount, headY * blockCount, blockSize, blockSize)
+    ctx.fillStyle = 'blueviolet';
+    for (let i = 0; i < snakeBody.length; i++) {
+        let body = snakeBody[i];
+        ctx.fillRect(body.x * blockCount, body.y * blockCount, blockSize, blockSize);
+    }
+
+    snakeBody.push(new Crawler(headX, headY));
+    while (snakeBody.length > tail) {
+        snakeBody.shift();
+    }
+
+    ctx.fillStyle = 'blueviolet';
+    ctx.fillRect(headX * blockCount, headY * blockCount, blockSize, blockSize);
+
 }
 
 function changePosition() {
@@ -46,62 +134,36 @@ document.body.addEventListener('keydown', keyDown);
 function keyDown(event) {
     // up
     if(event.keyCode == 38) {
+        if (ySpeed == 1)
+            return;
         ySpeed = -1;
         xSpeed = 0;
     }
     // down
     if(event.keyCode == 40) {
+        if (ySpeed == -1)
+            return;
         ySpeed = 1;
         xSpeed = 0;
     }
     // right
     if(event.keyCode == 39) {
+        if (xSpeed == -1)
+            return;
         ySpeed = 0;
         xSpeed = 1;
     }
     // left
     if(event.keyCode == 37) {
+        if (xSpeed == 1)
+            return;
         ySpeed = 0;
         xSpeed = -1;
     }
 }
 
+drawGame();
 
 // Set up for Canvas rendering
-// game.setAttribute('height', getComputedStyle(game)['height']);
-// game.setAttribute('width', getComputedStyle(game)['width']);
-
-
-// Entitites
-// class Crawler {
-//     constructor(x, y, color, width, height) {
-//         this.x = x;
-//         this.y = y;
-//         this.color = color;
-//         this.width = width;
-//         this.height = height;
-
-//         this.render = function() {
-//             context.fillstyle = this.color;
-//             context.fillRect(this.x, this.y, this.width, this.height);
-//         }
-//     }
-// }
-
-
-// window.onload = function() {
-//     board = document.getElementById('game');
-//     board.height = rows * blockSize;
-//     board.width = columns * blockSize;
-//     snake = new Crawler(10, 10, 'green', 10, 10);
-// }
-
-
-// food
-// let foodX;
-// let foodY;
-
-// function getFood() {
-//     foodX = Math.floor(Math.random() * columns) * blockSize;
-//     foodY = Math.floor(Math.random() * rows) * blockSize;
-// }
+game.setAttribute('height', getComputedStyle(game)['height']);
+game.setAttribute('width', getComputedStyle(game)['width']);
